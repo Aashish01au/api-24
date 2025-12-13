323deab608dc8150e44ee55ad8e37b7d451cc4fe
Aashish Sah <aashish01au@gmail.com>
2025-12-13 21:43:19 +0300

    File System

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const fs = require("fs")
-const fileName = "./user.json"
+let path = "./uploaders/files/"
+if(!fs.existsSync(path)){
+    fs.chmodSync(path,"0777") // Change Mode Directory
+    fs.chownSync(path,"sudo") // Change Owner Directory ---> sudo = admin
+    fs.mkdirSync(path,{recursive:true})
+}
+const fileName = path+"user.json"
+
 const users = [
     {
         name:"Aashish",
@@ -17,25 +24,65 @@ const users = [
         status:"active"
     }
 ]
-
+// array to string
 const userStr = JSON.stringify(users)
-
-fs.open(fileName,"w",(error,fd)=>{
-    if(error){
-        console.log(error)
-        console.log("Error Opening file")
-    }else{
-        //open 
-        fs.write(fd,userStr,(errorObj,bytes)=>{
-            if(errorObj){
-                console.log(errorObj)
-                console.log("Error while writing in file")
-            }else{
-                console.log("successfuly written"+bytes,"Butes")
-            }
-
-        })
-        fs.close(fd)
+const newUsers = [
+    {
+        name:"Aakash",
+        role:"admin",
+        status:"active"
+    },
+    {
+        name:"Ramesh",
+        role:"seller",
+        status:"inactive"
+    },
+    {
+        name:"Mumkesh",
+        role:"admin",
+        status:"inactive"
     }
-})
+]
+const newUserStr = JSON.stringify(newUsers)
+//# file write operations
+// open
+    // search ==> open , create open
+// write
+// close
+
+// ## Native System...
+// fs.open(fileName,"w",(error,fd) =>{
+//     if(error){
+//         console.log(error)
+//         console.log("Error opening file")
+//     }else{
+//         fs.write(fd,userStr,(errorObj,noBytes)=>{
+//             if(errorObj){
+//                 console.log(error)
+//                 console.log("Rrror writing file")
+//             }else{
+//                 console.log("Successfully writen "+noBytes+"Bytes")
+//             }
+//         })
+//         fs.close(fd)
+//     }
+// })
+// // core system...
+// const fd = fs.openSync(fileName,"w")
+// if(!fd){
+//     console.log("Error opening file")
+// }else{
+//     const noBytes = fs.writeSync(fd,userStr)
+//     if(!noBytes){
+//         console.log("Error writing in file")
+//     }
+//     fs.closeSync(fd)
+// }
+// operational System...
+fs.writeFileSync(fileName,userStr)
+//    console.log("file Writing Successfully..")
+let readData= fs.readFileSync(fileName,{encoding:"utf8"})
+readData= JSON.parse(readData)
+//console.log(typeof readData)
+console.log(readData)
 
--- readme.md
+++ readme.md
@@ -1,2 +1,19 @@
 ### EXPRESS APPLICATION API-24 :-
+# Permission in Application = 777
+dwrx(Delete Write Read Execute)
+    7 = dwrx
+    6 = wrx
+    5 = rx
+    0 = none -
 
+    Admin       Group       other/Guest
+    7              6           4
+    6              5           4
+
+# Foler = 765
+# fILE = 654
+
+## HTPP
+## REST API 
+## MVC PATTERN
+## EXPRESS 
